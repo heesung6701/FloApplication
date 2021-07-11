4f0f055d3161e8f5d396d13c742bea1c6a4a38d3
윤희성 <heesung6701@naver.com>
2021-07-11 18:31:24 +0900

    Add MusicInfoViewModel and layout

---
+++ app/src/main/java/com/quokkaman/floapplication/viewmodel/MusicInfoViewModel.kt
@@ -0,0 +1,30 @@
+package com.quokkaman.floapplication.viewmodel
+
+import android.view.View
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.Transformations
+import androidx.lifecycle.ViewModel
+import com.quokkaman.floapplication.R
+import com.quokkaman.floapplication.model.Song
+
+class MusicInfoViewModel : ViewModel() {
+
+    private val songLiveData = MutableLiveData<Song>()
+
+    val titleLiveData: LiveData<String> = Transformations.map(songLiveData) { song -> song.title }
+    val singerLiveData: LiveData<String> = Transformations.map(songLiveData) { song -> song.singer }
+    val imageLiveData: LiveData<String> = Transformations.map(songLiveData) { song -> song.imageUrl }
+
+    fun update(song: Song) {
+        songLiveData.value = song
+    }
+
+    fun onClick(view: View) {
+        when (view.id) {
+            R.id.tv_singer -> {
+
+            }
+        }
+    }
+}
