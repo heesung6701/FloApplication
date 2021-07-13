6ec46553da5bcbe11f540e1ce07536915d2d4da9
윤희성 <heesung6701@naver.com>
2021-07-13 23:43:42 +0900

    Update Main Activity

--- app/src/main/java/com/quokkaman/floapplication/MainActivity.kt
+++ app/src/main/java/com/quokkaman/floapplication/MainActivity.kt
@@ -1,12 +1,143 @@
 package com.quokkaman.floapplication
 
-import androidx.appcompat.app.AppCompatActivity
+import android.media.MediaPlayer
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import android.os.Message
+import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.ViewModelProvider
+import com.quokkaman.floapplication.databinding.ActivityMainBinding
+import com.quokkaman.floapplication.dto.SongDTO
+import com.quokkaman.floapplication.repository.SongRepository
+import com.quokkaman.floapplication.viewmodel.MediaControllerViewModel
+import com.quokkaman.floapplication.viewmodel.MusicInfoViewModel
+import com.quokkaman.floapplication.viewmodel.MusicLyricViewModel
+import com.quokkaman.floapplication.viewmodel.SeekbarViewModel
+import io.reactivex.android.schedulers.AndroidSchedulers
+import io.reactivex.disposables.CompositeDisposable
+import io.reactivex.observers.DisposableSingleObserver
+import io.reactivex.schedulers.Schedulers
 
 class MainActivity : AppCompatActivity() {
+
+    companion object {
+        const val MESSAGE_WHAT_SECOND = 1
+        const val MESSAGE_WHAT_MILLISECOND = 2
+    }
+
+    private lateinit var binding: ActivityMainBinding
+
+    private val songRepository = SongRepository
+    private val disposable = CompositeDisposable()
+    private lateinit var musicInfoViewModel : MusicInfoViewModel
+    private lateinit var musicLyricViewModel : MusicLyricViewModel
+    private lateinit var seekbarViewModel : SeekbarViewModel
+    private lateinit var mediaControllerViewModel : MediaControllerViewModel
+
+    private val mediaPlayer = MediaPlayer()
+
+    private var isPlaying = false
+
+    private val handler = object: Handler(Looper.getMainLooper()) {
+        override fun handleMessage(msg: Message) {
+            when(msg.what) {
+                MESSAGE_WHAT_MILLISECOND -> {
+                    val playMillisecond : Int = msg.data.getInt("millisecond")
+                    musicLyricViewModel.updateSecond(playMillisecond)
+                }
+                MESSAGE_WHAT_SECOND -> {
+                    val playSecond : Int = msg.data.getInt("second")
+                    seekbarViewModel.playSecond.value = playSecond
+                }
+            }
+        }
+    }
+
+    var recentSecond = -1
+    var recentMillisecond = -1
+    private val seekbarThread = object: Thread() {
+        override fun run() {
+            while(true) {
+                if(mediaPlayer.isPlaying) {
+                    val millisecond = mediaPlayer.currentPosition
+                    if (recentMillisecond == millisecond) continue
+                    recentMillisecond = millisecond
+                    handler.sendMessage(Message().apply {
+                        what = MESSAGE_WHAT_MILLISECOND
+                        data.putInt("millisecond", millisecond)
+                    })
+                    val second = millisecond / 1000
+                    if (second == recentSecond) continue
+                    recentSecond = second
+                    handler.sendMessage(Message().apply {
+                        what = MESSAGE_WHAT_SECOND
+                        data.putInt("second", second)
+                    })
+                }
+            }
+        }
+    }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
 
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        musicInfoViewModel = ViewModelProvider(this).get(MusicInfoViewModel::class.java)
+        musicLyricViewModel = ViewModelProvider(this).get(MusicLyricViewModel::class.java)
+        seekbarViewModel = ViewModelProvider(this).get(SeekbarViewModel::class.java)
+        mediaControllerViewModel = ViewModelProvider(this).get(MediaControllerViewModel::class.java)
+
+        binding.musicInfoViewModel = musicInfoViewModel
+        binding.musicLyricViewModel = musicLyricViewModel
+        binding.seekbarViewModel = seekbarViewModel
+        binding.mediaControllerViewModel = mediaControllerViewModel
+        binding.lifecycleOwner = this
+
+        fetchMusicInfo()
+
+        mediaControllerViewModel.play.observe(this, {
+            isPlaying = it
+            if(isPlaying) {
+                mediaPlayer.start()
+            } else {
+                mediaPlayer.pause()
+            }
+        })
+        seekbarThread.start()
+    }
+
+    private fun fetchMusicInfo() {
+        disposable.add(
+            songRepository.getSong()
+                .subscribeOn(Schedulers.newThread())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribeWith(object : DisposableSingleObserver<SongDTO>() {
+                    override fun onSuccess(t: SongDTO?) {
+                        val song = t?.toModel() ?: return
+                        musicInfoViewModel.update(song)
+                        mediaPlayer.setDataSource(t.file)
+                        mediaPlayer.prepare()
+                        val duration = mediaPlayer.duration
+                        seekbarViewModel.totalSecond.value = duration / 1000
+                        musicLyricViewModel.updateLyricLine(song.lyricLineList, duration)
+
+                        mediaControllerViewModel.play.value = true
+                    }
+
+                    override fun onError(e: Throwable?) {
+                        e?.printStackTrace()
+                    }
+                })
+        )
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        disposable.clear()
+        mediaPlayer.stop()
+        isPlaying = false
+        seekbarThread.interrupt()
     }
 }
