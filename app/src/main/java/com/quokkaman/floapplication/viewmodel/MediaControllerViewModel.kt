14dce68daaffab0f4db5b84ef58c30d717d88acd
윤희성 <heesung6701@naver.com>
2021-07-14 20:46:33 +0900

    implement MediaControllerViewModel

--- app/src/main/java/com/quokkaman/floapplication/viewmodel/MediaControllerViewModel.kt
+++ app/src/main/java/com/quokkaman/floapplication/viewmodel/MediaControllerViewModel.kt
@@ -1,35 +1,57 @@
 package com.quokkaman.floapplication.viewmodel
 
-import android.view.View
+import android.media.MediaPlayer
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
-import com.quokkaman.floapplication.R
 
 class MediaControllerViewModel : ViewModel() {
-    val play = MutableLiveData(false)
-    val repeat = MutableLiveData(false)
-    val shuffle = MutableLiveData(false)
 
-    fun onClick(view: View) {
-        when (view.id) {
-            R.id.repeat_btn -> {
-                repeat.value = repeat.value == false
-            }
-            R.id.prev_btn -> {
+    enum class RepeatType {
+        REPEAT_TYPE_NONE,
+        REPEAT_TYPE_ALL,
+        REPEAT_TYPE_ONE
+    }
 
-            }
-            R.id.play_btn -> {
-                play.value = play.value == false
-            }
-            R.id.next_btn -> {
+    fun RepeatType.next(): RepeatType {
+        val values = RepeatType.values()
+        return values[(this.ordinal + 1) % values.size]
+    }
 
-            }
-            R.id.shuffle_btn -> {
-                shuffle.value = shuffle.value == false
-            }
-            R.id.list_btn -> {
+    val playingLiveData = MutableLiveData(false)
+    val repeatLivaData = MutableLiveData(RepeatType.REPEAT_TYPE_NONE)
+    val shuffleLiveData = MutableLiveData(false)
+    var mediaPlayer: MediaPlayer? = null
 
-            }
-        }
+    fun play() {
+        if (playingLiveData.value == true) return
+        playingLiveData.value = true
+        mediaPlayer?.start()
+    }
+
+    fun pause() {
+        if (playingLiveData.value == false) return
+        playingLiveData.value = false
+        mediaPlayer?.pause()
+    }
+
+    fun toggleShuffle() {
+        shuffleLiveData.value = (shuffleLiveData.value == false)
+    }
+
+    fun toggleRepeat() {
+        repeatLivaData.value = repeatLivaData.value?.next() ?: RepeatType.REPEAT_TYPE_NONE
+        mediaPlayer?.isLooping = repeatLivaData.value == RepeatType.REPEAT_TYPE_ONE
+    }
+
+    fun prevMusic() {
+
+    }
+
+    fun nextMusic() {
+
+    }
+
+    fun showList() {
+
     }
 }
