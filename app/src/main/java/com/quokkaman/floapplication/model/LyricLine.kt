bd9a4e4d61c759b48bbf7a320338d333e75c4921
윤희성 <heesung6701@naver.com>
2021-07-16 14:43:04 +0900

    Add Parcelable to model

--- app/src/main/java/com/quokkaman/floapplication/model/LyricLine.kt
+++ app/src/main/java/com/quokkaman/floapplication/model/LyricLine.kt
@@ -1,6 +1,34 @@
 package com.quokkaman.floapplication.model
 
+import android.os.Parcel
+import android.os.Parcelable
+
 data class LyricLine(
     val msec: Int,
     val lyrics: String
-)
+) : Parcelable {
+    constructor(parcel: Parcel) : this(
+        parcel.readInt(),
+        parcel.readString()!!
+    )
+
+    override fun writeToParcel(parcel: Parcel, flags: Int) {
+        parcel.writeInt(msec)
+        parcel.writeString(lyrics)
+    }
+
+    override fun describeContents(): Int {
+        return 0
+    }
+
+    companion object CREATOR : Parcelable.Creator<LyricLine> {
+        override fun createFromParcel(parcel: Parcel): LyricLine {
+            return LyricLine(parcel)
+        }
+
+        override fun newArray(size: Int): Array<LyricLine?> {
+            return arrayOfNulls(size)
+        }
+    }
+
+}
--- app/src/main/java/com/quokkaman/floapplication/model/Song.kt
+++ app/src/main/java/com/quokkaman/floapplication/model/Song.kt
@@ -1,5 +1,8 @@
 package com.quokkaman.floapplication.model
 
+import android.os.Parcel
+import android.os.Parcelable
+
 data class Song(
     val singer: String,
     val album: String,
@@ -8,4 +11,45 @@ data class Song(
     val fileUrl: String,
     val imageUrl: String,
     val lyricLineList: List<LyricLine>
-)
+) : Parcelable {
+
+    constructor(parcel: Parcel) : this(
+        parcel.readString()!!,
+        parcel.readString()!!,
+        parcel.readString()!!,
+        parcel.readInt(),
+        parcel.readString()!!,
+        parcel.readString()!!,
+        parcel.createTypedArrayList(LyricLine)!!
+    ) {
+    }
+
+    override fun writeToParcel(parcel: Parcel, flags: Int) {
+        parcel.writeString(singer)
+        parcel.writeString(album)
+        parcel.writeString(title)
+        parcel.writeInt(duration)
+        parcel.writeString(fileUrl)
+        parcel.writeString(imageUrl)
+        parcel.writeTypedList(lyricLineList)
+    }
+
+    override fun describeContents(): Int {
+        return 0
+    }
+
+    companion object {
+        val INTENT_KEY = "SONG"
+
+        @JvmField
+        val CREATOR = object: Parcelable.Creator<Song> {
+            override fun createFromParcel(parcel: Parcel): Song {
+                return Song(parcel)
+            }
+
+            override fun newArray(size: Int): Array<Song?> {
+                return arrayOfNulls(size)
+            }
+        }
+    }
+}
